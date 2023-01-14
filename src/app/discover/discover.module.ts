4a51272c5cbadf352ae9c9ebf6e8b18e40e140fa
DESKTOP-RUMMCRL\pinza <pinzariurazvann@gmail.com>
2023-01-14 18:39:46 +0200

    Finished half of the search bar.

--- src/app/discover/discover.module.ts
+++ src/app/discover/discover.module.ts
@@ -7,9 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { Discover } from './discover.component';
 import { DiscoverRoutingModule } from './discover-routing.module';
 import { Navbar } from '../navbar/navbar.component';
+import { SearchBar } from '../searchBar/searchBar.component';
 
 @NgModule({
-  declarations: [Discover, Navbar],
+  declarations: [Discover, Navbar, SearchBar],
   imports: [
     CommonModule,
     FormsModule,
