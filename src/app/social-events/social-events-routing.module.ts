b262455fcfb80c87374a5ec0f2de0d58dd116cd2
George Coblisan <geocoblisan@gmail.com>
2023-01-15 16:12:05 +0200

    refactoring

--- src/app/social-events/social-events-routing.module.ts
+++ src/app/social-events/social-events-routing.module.ts
@@ -3,28 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CreateEventComponent } from './create-event/create-event.component';
 import { Discover } from './discover/discover.component';
-import { EventDetailsComponent } from './event-details/event-details.component';
-import { EventForumComponent } from './event-forum/event-forum.component';
-import { EventsActivityComponent } from './events-activity/events-activity.component';
 import { EventsCalendarComponent } from './events-calendar/events-calendar.component';
 import { EventsFeedComponent } from './events-feed/events-feed.component';
-import { EventsHomePageComponent } from './events-home-page/events-home-page.component';
 import { EventsJoinComponent } from './events-join/events-join.component';
 import { EventsMapComponent } from './events-map/events-map.component';
 import { EventsRewardComponent } from './events-reward/events-reward.component';
-import { EventsSearchComponent } from './events-search/events-search.component';
 import { SelectCategory } from './selectCategory/selectCategory.component';
 
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: EventsHomePageComponent,
-  },
-  {
-    path: 'search',
-    component: EventsSearchComponent,
-  },
   {
     path: 'feed',
     component: EventsFeedComponent,
@@ -42,24 +29,12 @@ const routes: Routes = [
     component: EventsCalendarComponent,
   },
   {
-    path: 'event-details',
-    component: EventDetailsComponent,
-  },
-  {
     path: 'create',
     component: CreateEventComponent,
   },
   {
     path: 'reward',
     component: EventsRewardComponent,
-  },
-  {
-    path: 'activity',
-    component: EventsActivityComponent,
-  },
-  {
-    path: 'event-forum',
-    component: EventForumComponent,
   },
   {
     path: 'select-category',
--- src/app/social-events/social-events.module.ts
+++ src/app/social-events/social-events.module.ts
@@ -6,16 +6,11 @@ import { GoogleMapsModule } from '@angular/google-maps';
 
 import { SocialEventsRoutingModule } from './social-events-routing.module';
 import { CreateEventComponent } from './create-event/create-event.component';
-import { EventDetailsComponent } from './event-details/event-details.component';
-import { EventsActivityComponent } from './events-activity/events-activity.component';
 import { EventsCalendarComponent } from './events-calendar/events-calendar.component';
 import { EventsFeedComponent } from './events-feed/events-feed.component';
-import { EventsHomePageComponent } from './events-home-page/events-home-page.component';
 import { EventsJoinComponent } from './events-join/events-join.component';
 import { EventsMapComponent } from './events-map/events-map.component';
 import { EventsRewardComponent } from './events-reward/events-reward.component';
-import { EventsSearchComponent } from './events-search/events-search.component';
-import { EventForumComponent } from './event-forum/event-forum.component';
 import { SearchBar } from './searchBar/searchBar.component';
 import { Discover } from './discover/discover.component';
 import { SelectCategory } from './selectCategory/selectCategory.component';
@@ -35,16 +30,11 @@ import {FullCalendarModule} from "@fullcalendar/angular";
   ],
   declarations: [
     CreateEventComponent,
-    EventDetailsComponent,
-    EventsActivityComponent,
     EventsCalendarComponent,
     EventsFeedComponent,
-    EventsHomePageComponent,
     EventsJoinComponent,
     EventsMapComponent,
     EventsRewardComponent,
-    EventsSearchComponent,
-    EventForumComponent,
     SearchBar,
     Discover,
     SelectCategory,
@@ -52,16 +42,11 @@ import {FullCalendarModule} from "@fullcalendar/angular";
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [
     CreateEventComponent,
-    EventDetailsComponent,
-    EventsActivityComponent,
     EventsCalendarComponent,
     EventsFeedComponent,
-    EventsHomePageComponent,
     EventsJoinComponent,
     EventsMapComponent,
     EventsRewardComponent,
-    EventsSearchComponent,
-    EventForumComponent,
     SearchBar,
     Discover,
     SelectCategory,
